a10c77444e62aa06e5b9d3c30922d7de50237f56
miezuit <miezuit@gmail.com>
2022-02-01 19:08:24 +0200

    tema cerc

--- dreptunghi/d.js
+++ dreptunghi/d.js
@@ -1,6 +1,8 @@
 var dr = document.querySelector('#dreptunghi')
+var cerc = document.querySelector('#cerc')
 
 dr.addEventListener('click', minimize)
+cerc.addEventListener('click', move)
 
 async function minimize() {
     for(let i = 10; i >= 0; i--) {
@@ -8,4 +10,13 @@ async function minimize() {
         await new Promise(r => setTimeout(r, 20))
         dr.style.height = i + 'em'
     }
+}
+
+async function move() {
+    for(let i = 10; i <= 200; i++) {
+        // asteptam 20 de ms:
+        await new Promise(r => setTimeout(r, 20))
+        cerc.style.top = cerc.style.left = i + 'px'
+    }
+    cerc.removeEventListener('click', move)
 }
